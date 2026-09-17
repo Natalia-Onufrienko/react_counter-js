d8f2823e58a1df6a09fa9f0a9fe3600a4e3be929
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-09-17 12:54:22 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,25 @@ import './App.scss';
 
 export const App = () => {
   const [count, setCount] = useState(0);
-
-  // Коректна реалізація через функціональне оновлення стану
   const addOne = () => {
     setCount(prevCount => prevCount + 1);
   };
 
-  // Коректна реалізація через функціональне оновлення стану
   const add100 = () => {
     setCount(prevCount => prevCount + 100);
   };
 
   const increase = () => {
+    addOne();
     if (count % 5 === 0) {
       add100();
     }
-
-    addOne();
   };
 
   return (
     <div className="App">
-      <h1 className="App__title">{`Count: ${count}`}</h1>
+      {' '}
+      <h1 className="App__title">Count: {count}</h1>
       <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
